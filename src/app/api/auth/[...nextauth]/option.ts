c9d0695bb337cf49f12d797f27e09660eb5d5935
sanjaraiy <deepakrai78788@gmail.com>
2025-08-20 22:24:14 +0530

    added setup google auth

--- src/app/api/auth/[...nextauth]/option.ts
+++ src/app/api/auth/[...nextauth]/option.ts
@@ -1,9 +1,49 @@
-import { AuthOptions } from "next-auth";
+import { AuthOptions, ISODateString } from "next-auth";
+import { JWT } from "next-auth/jwt";
 import Google from "next-auth/providers/google";
+
+export interface CustomSession {
+  user?: CustomUser;
+  expires: ISODateString;
+
+}
+
+
+export interface CustomUser {  
+    id?: string | null;
+    name?: string | null;
+    email?: string | null;
+    image?: string | null;
+    provider?: string | null;
+    token?: string | null;
+ }
 
 export const authOption: AuthOptions = {
   pages: {
     signIn: "/",
+  },
+  callbacks: {
+    async signIn({user, account}){
+        console.log("The user data is", user);
+        console.log("The account is", account);
+        return true;
+    },
+
+
+
+
+    async session({session, user, token}: {session: CustomSession, user: CustomUser, token: JWT}){
+        session.user = token.user as CustomUser;
+        return session;
+    },
+    async jwt({token, user}){
+        if(user){
+            token.user = user
+        }
+
+        return token;
+    }
+
   },
   providers: [
     Google({
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,13 +3,18 @@ import Footer from "@/components/base/Footer";
 import HeroSection from "@/components/base/HeroSection";
 import Navbar from "@/components/base/Navbar";
 import UserReviews from "@/components/base/UserReviews";
+import { getServerSession } from "next-auth";
 import Image from "next/image";
+import { authOption, CustomSession } from "./api/auth/[...nextauth]/option";
 
-export default function Home() {
+export default async function Home() {
+  
+  const session: CustomSession | null = await getServerSession(authOption);
+
   return (
      <div className="min-h-screen flex flex-col ">
       {/* Header */}
-      <Navbar />
+      <Navbar user={session?.user} />
       {/* Hero Section */}
       <HeroSection />
 
